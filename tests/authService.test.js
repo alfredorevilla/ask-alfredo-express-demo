cd74f09f546ec72845ed07c1201de67ad23fdb93
Alfredo Revilla <alfredorevilla@gmail.com>
2018-05-23 17:52:21 -0500

    Re-ordered authService tests.

--- tests/authService.test.js
+++ tests/authService.test.js
@@ -1,21 +1,27 @@
 const expect = require('chai').expect;
 const userStore = {};
-const passwordValidator = {}
-const authService = new (require('../services/authService'))(userStore, passwordValidator);
+const passwordHasher = {}
+const authService = new (require('../services/authService'))(userStore, passwordHasher);
 
-describe('AuthService.login', () => {
-    it('returns false if no user found', async () => {
-        userStore.getByEmail = () => null;
-        expect(await authService.login('', '')).to.be.false;
+describe('authService', () => {
+
+    describe('login', () => {
+
+        it('returns false if no user found', async () => {
+            userStore.getByEmail = () => null;
+            expect(await authService.login('', '')).to.be.false;
+        });
+        it('returns false on invalid password', async () => {
+            userStore.getByEmail = () => { return {} };
+            userStore.getHashedPassword = () => '';
+            passwordHasher.verifyHashedPassword = () => false;
+            expect(await authService.login('', '')).to.be.false;
+        });
+        it('returns true on valid password', async () => {
+            passwordHasher.verifyHashedPassword = () => true;
+            expect(await authService.login('', '')).to.be.true;
+        });
+    
     });
-    it('returns false on invalid password', async () => {
-        userStore.getByEmail = () => { return { } };
-        userStore.getHashedPassword = () => '';
-        passwordValidator.verifyHashedPassword = () => false;
-        expect(await authService.login('', '')).to.be.false;
-    });
-    it('returns true on valid password', async () => {
-        passwordValidator.verifyHashedPassword = () => true;
-        expect(await authService.login('', '')).to.be.true;
-    });
+
 });
