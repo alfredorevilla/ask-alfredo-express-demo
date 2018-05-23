ffd1ba19c80736bf6a0b3cc1ba48151e9a936dc7
Alfredo Revilla <alfredorevilla@gmail.com>
2018-05-23 01:22:52 -0500

    Todos comments.

--- services/consumerService.js
+++ services/consumerService.js
@@ -1,7 +1,9 @@
 const users = [];
 
+//  todo: make methods async
 module.exports = {
     add(model) {
+        //  todo: replace with validation lib
         if (!model || !model.name || !model.password || !model.email)
             throw Error('Invalid user');
         users.push(model);
