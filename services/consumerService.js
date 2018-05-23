aa2e26baf3f9b2aaa7c167c3e31c0a91525fd382
Alfredo Revilla <alfredorevilla@gmail.com>
2018-05-23 01:32:16 -0500

    sendEmailMessage func.

--- services/consumerService.js
+++ services/consumerService.js
@@ -1,3 +1,5 @@
+const sendEmailMessage = require('./sendEmailMessage');
+
 const users = [];
 
 //  todo: make methods async
@@ -7,6 +9,7 @@ module.exports = {
         if (!model || !model.name || !model.password || !model.email)
             throw Error('Invalid user');
         users.push(model);
+        sendEmailMessage(model.email, 'Created', 'Your account has been created');
     },
     get() {
         return users;
---
+++ services/sendEmailMessage.js
@@ -0,0 +1,2 @@
+module.exports = (dest, subject, message) => console.info(`Mail with subject '${subject}' and message '${message}' has been sent to '${dest}'`)
+
