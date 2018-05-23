010bd654da73376792cf6c3479ec8f34347ed6bc
Alfredo Revilla <alfredorevilla@gmail.com>
2018-05-23 00:36:18 -0500

    userController.put stub.

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,6 +4,10 @@ var userController = express.Router();
 
 userController.get('/', (req, res) => res.send('users'));
 
+userController.post('/', (req, res) => {
+    res.status(200).end();
+});
+
 module.exports = userController;
 
 
