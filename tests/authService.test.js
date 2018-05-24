211e92c03124f4dba5c5d01ecad4993bc8282e8d
Alfredo Revilla <alfredorevilla@gmail.com>
2018-05-23 20:20:08 -0500

    Formatting.

--- tests/authService.test.js
+++ tests/authService.test.js
@@ -11,17 +11,19 @@ describe('authService', () => {
             userStore.getByEmail = () => null;
             expect(await authService.login('', '')).to.be.false;
         });
+        
         it('returns false on invalid password', async () => {
             userStore.getByEmail = () => { return {} };
             userStore.getHashedPassword = () => '';
             passwordHasher.verifyHashedPassword = () => false;
             expect(await authService.login('', '')).to.be.false;
         });
+        
         it('returns true on valid password', async () => {
             passwordHasher.verifyHashedPassword = () => true;
             expect(await authService.login('', '')).to.be.true;
         });
-    
+
     });
 
 });
