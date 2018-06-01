db87313c6b466506a60f6be760565c19eb8b35ff
Alfredo Revilla <alfredorevilla@gmail.com>
2018-06-01 17:52:21 -0500

    handleAsyncError comment.

--- controllers/handleAsyncError.js
+++ controllers/handleAsyncError.js
@@ -1,3 +1,6 @@
+/*
+    By design express won't respond with a 500 status code for  rejected promises so we must wrapper any call to and handle the error to the next middleware.
+*/
 module.exports = action =>
     async (req, res, next) => {
         try {
