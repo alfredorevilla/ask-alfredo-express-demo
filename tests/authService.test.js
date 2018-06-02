8351022dcdaeb3c7a6389aa09e722e494a51c4ff
Alfredo Revilla <alfredorevilla@gmail.com>
2018-06-02 17:54:00 -0500

    Fixed auth service test.

--- tests/authService.test.js
+++ tests/authService.test.js
@@ -3,7 +3,7 @@
 const expect = require('chai').expect;
 const userStore = {};
 const passwordHasher = {}
-const authService = new (require('../services/authService'))(userStore, passwordHasher);
+const authService = require('../services/authService')(userStore, passwordHasher);
 
 describe('authService', () => {
 
@@ -13,14 +13,14 @@ describe('authService', () => {
             userStore.getByEmail = () => null;
             expect(await authService.login('', '')).to.be.false;
         });
-        
+
         it('returns false on invalid password', async () => {
             userStore.getByEmail = () => { return {} };
             userStore.getHashedPassword = () => '';
             passwordHasher.verifyHashedPassword = () => false;
             expect(await authService.login('', '')).to.be.false;
         });
-        
+
         it('returns true on valid password', async () => {
             passwordHasher.verifyHashedPassword = () => true;
             expect(await authService.login('', '')).to.be.true;
