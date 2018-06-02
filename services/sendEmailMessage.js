5dad8414df1d6ae6210cc0397b779649fbeca417
Alfredo Revilla <alfredorevilla@gmail.com>
2018-06-01 22:13:35 -0500

    sendEmailMessage and config and tests.

--- services/sendEmailMessage.js
+++ services/sendEmailMessage.js
@@ -1,2 +1,8 @@
-module.exports = (dest, subject, message) => console.info(`Mail with subject '${subject}' and message '${message}' has been sent to '${dest}'`)
+const config = require('./sendEmailMessage.config.json');
+const apikey = config.apikeys[process.env['NODE_ENV']];
+
+/*
+    todo: made log function injectable so it's test friendly
+*/
+module.exports = (dest, subject, message, log) => console.info(apikey ? `Mail text using api key ${apikey} and arguments ${JSON.stringify({ dest, subject, message })}` : 'No apikey set, cannot send email.')
 
